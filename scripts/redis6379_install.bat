d1269008e0d60e888d2611352433ad3788991910
uniqs <uniqs@163.com>
2021-06-10 21:21:20 +0800

    use 7z format for higher compress ratio

--- scripts/redis6379_install.bat
+++ scripts/redis6379_install.bat
@@ -2,7 +2,7 @@ set INSTALL_PATH=..\install\redis6379\
 
 set CURR_DIR=%~dp0
 
-7z x ..\zips\Redis-x64-5.0.10.zip -o%INSTALL_PATH% -aoa
+7z x ..\zips\Redis-x64-5.0.10.7z -o%INSTALL_PATH% -aoa
 cd %INSTALL_PATH%
 set R6379PATH=%~dp0
 echo %R6379PATH%
