dd5adac7c6df17418089c2811b2dcde42adac84e
uniqs <uniqs@163.com>
2021-06-10 22:22:28 +0800

    hello world

--- scripts/mysql5733_uninstall.bat
+++ scripts/mysql5733_uninstall.bat
@@ -1,10 +1,15 @@
 set INSTALL_PATH=..\install\mysql5733\
 
+set ROOT_DIR=%~dp0
+
 cd %INSTALL_PATH%
 
 cd bin
+
+net stop mysql
+
 mysqld --remove
 
-cd ..\..\scripts\
+cd %ROOT_DIR%
 
 rd /S /Q %INSTALL_PATH%
--- scripts/redis6379_uninstall.bat
+++ scripts/redis6379_uninstall.bat
@@ -1,10 +1,12 @@
 set INSTALL_PATH=..\install\redis6379\
 
+set ROOT_DIR=%~dp0
+
 cd %INSTALL_PATH%
 
 .\redis-server --service-stop --service-name redis6379
 .\redis-server --service-uninstall --service-name redis6379
 
-cd ..\..\scripts\
+cd %ROOT_DIR%
 
 rd /S /Q %INSTALL_PATH%
